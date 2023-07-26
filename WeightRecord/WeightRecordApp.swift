6935b3ddd42819e476665d79843c1a900be2866d
rejxcy <rejxcy0903@gmail.com>
2023-07-26 12:55:08 +0800

    Project initialization

---
+++ WeightRecord/WeightRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightRecordApp.swift
+//  WeightRecord
+//
+//  Created by 胡志龍 on 2023/7/26.
+//
+
+import SwiftUI
+
+@main
+struct WeightRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
