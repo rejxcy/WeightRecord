6924ea192f71e8ec50d29138837e695d41baf31d
rejxcy <rejxcy0903@gmail.com>
2023-07-26 12:58:57 +0800

    add WeightRecord struct

---
+++ WeightRecord/Model/WeightReocrd.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct WeightRecord {
+    let date: Date
+    let weight: Double
+
+    init(date: Date, weight: Double) {
+        self.date = date
+        self.weight = weight
+    }
+}
